ff420c1c1692d766968f4670eb7bc1ce1f214195
saumanarsad <sauman.arshad@gmail.com>
2025-02-09 03:28:24 +0500

    test api

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/index', function () {
+    return response()->json(['Message' => "hello"]);
 });
