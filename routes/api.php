764947925122f4ad22d509980e980a7ab8c6df7d
saumanarsad <sauman.arshad@gmail.com>
2025-02-18 01:48:01 +0500

    Add sanctum guard

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
-Route::post('signup', function () {
-    dd(request());
+use function Laravel\Prompts\error;
+
+Route::post('signup', function (Request $request) {
+
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+    $user = User::whereEmail($request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        $error =  ValidationException::withMessages([
+            'email' => 'The provided credentials are incorrect'
+        ]);
+        throw $error;
+    }
+
+    return $user->createToken($request->header('user-agent'))->plainTextToken;
 });
