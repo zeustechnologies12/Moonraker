8e1d942da09ca4eefab077387672a81b0d0400dd
saumanarsad <sauman.arshad@gmail.com>
2025-03-02 21:02:37 +0500

    Added signup api

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,34 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
 use function Laravel\Prompts\error;
 
+
 Route::post('signup', function (Request $request) {
+    $validation = Validator::make($request->all(), [
+        'name' => 'required|string|unique:users,name',
+        'email' => 'required|email|unique:users,email',
+        "password" => 'required',
+    ]);
+    if ($validation->fails()) {
+        return response()->json([$validation->errors()]);
+    }
+
+    $user =  User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => $request->password
+    ]);
+    return response()->json([
+        'message' => 'User Created Successfully',
+        'user' => $user
+    ]);
+});
+
+Route::post('login', function (Request $request) {
 
     $request->validate([
         'email' => 'required|email',
