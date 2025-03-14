c14b6a51caac2e69a7089133b4adec3cf92c2a26
saumanarsad <sauman.arshad@gmail.com>
2025-03-15 03:14:35 +0500

    Created user login action

---
+++ app/Actions/UserLoginAction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Role;
+use App\Models\User;
+use App\Traits\CustomValidations;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class UserLoginAction
+{
+    use AsAction, CustomValidations;
+
+    public function handle(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'role' => 'required|string'
+        ]);
+        $user = User::whereEmail($request->email)->first();
+        $this->validateRole($request->role);
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'wrong credentials' => 'Your email or password is incorrect'
+            ]);
+        }
+        if ($user->role->name != $request->role) {
+            throw ValidationException::withMessages([
+                'wrong role' => 'Your role is incorrect for the user'
+            ]);
+        }
+
+        return $user->createToken($request->header('user-agent'))->plainTextToken;
+    }
+}
