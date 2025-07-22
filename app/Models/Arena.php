40ebca80206783d085b193612ad59ca2e6a9ea7f
Sauman007+ <somi@ITs-MacBook-Pro-53.local>
2025-07-23 01:07:08 +0500

    Model methods

--- app/Models/Arena.php
+++ app/Models/Arena.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Arena extends Model
 {
@@ -17,8 +19,13 @@ class Arena extends Model
     {
         return $this->belongsTo(Location::class);
     }
-    public function users()
+    public function users():BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function bookings():HasMany
+    {
+        return $this->hasMany(Booking::class);
     }
 }
--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Booking extends Model
 {
-     public function user(): BelongsTo
+    use SoftDeletes;
+    protected $fillable = [
+        'starts_at',
+        'ends_at',
+    ];
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
